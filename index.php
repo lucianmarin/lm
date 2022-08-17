95fad4ba3c32251cca73643cf8c7e69b6a1428e2
Lucian Marin <marin.lucian@gmail.com>
2022-08-18 00:56:49 +0300

    round svg icons

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<link rel="apple-touch-icon" type="image/png" href="/static/plus.png">
 	<link rel="icon" sizes="192x192" type="image/png" href="/static/plus.png">
 	<link rel="manifest" type="application/json" href="/static/manifest.json">
-	<link rel="stylesheet" type="text/css" href="/static/style.css?v=11">
+	<link rel="stylesheet" type="text/css" href="/static/style.css?v=12">
 	<link rel="alternate" type="application/rss+xml" title="Lucian Marin" href="https://feeds.feedburner.com/lucianmarin">
 </head>
 <body>
@@ -20,7 +20,7 @@
 		<div class="center">
 			<p class="meta">Hello</p>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24" class="purple">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 				</svg>
 				<p>
@@ -36,7 +36,7 @@
 		<div class="center">
 			<p class="meta">Web</p>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M4.871 4A17.926 17.926 0 003 12c0 2.874.673 5.59 1.871 8m14.13 0a17.926 17.926 0 001.87-8c0-2.874-.673-5.59-1.87-8M9 9h1.246a1 1 0 01.961.725l1.586 5.55a1 1 0 00.961.725H15m1-7h-.08a2 2 0 00-1.519.698L9.6 15.302A2 2 0 018.08 16H8" />
 				</svg>
 				<p>
@@ -47,7 +47,7 @@
 				<p>Multiline math calculator.</p>
 			</div>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M13 10V3L4 14h7v7l9-11h-7z" />
 				</svg>
 				<p>
@@ -58,7 +58,7 @@
 				<p>Built for speed. Engineered to last.</p>
 			</div>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
 					<path d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
 				</svg>
@@ -75,7 +75,7 @@
 		<div class="center">
 			<p class="meta">Android</p>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24" class="purple">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M13 10V3L4 14h7v7l9-11h-7z" />
 				</svg>
 				<p>
@@ -91,34 +91,34 @@
 		<div class="center">
 			<p class="meta">Mac</p>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 				</svg>
 				<p>
 					<a href="https://app.box.com/s/xu1pwny4qsx4h0nj0jrlsvyfmorjzn0q">
-						<strong>Monochrome 5.2</strong>
+						<strong>Monochrome<sup>5.2</sup></strong>
 					</a>
 				</p>
 				<p>Mobile browser for Mac.</p>
 			</div>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" />
 				</svg>
 				<p>
 					<a href="https://app.box.com/s/xvwan1mqk47v7rd6r7xbas12lu66v76p">
-						<strong>Multicolor 1.1</strong>
+						<strong>Multicolor<sup>1.1</sup></strong>
 					</a>
 				</p>
 				<p>Color schemes generator for Mac.</p>
 			</div>
 			<div class="product">
-				<svg viewBox="0 0 24 24" width="24" height="24">
+				<svg viewBox="0 0 24 24" width="16" height="16">
 					<path d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
 				</svg>
 				<p>
 					<a href="https://app.box.com/s/514y1vlpfkcuoh0a4lc226eizxhyb668">
-						<strong>Bicolor 1.2</strong>
+						<strong>Bicolor<sup>1.2</sup></strong>
 					</a>
 				</p>
 				<p>Contrast comparison tool for Mac.</p>
