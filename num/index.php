a6562e9af37bd2b8bd3c9cafa09a68f005dccc1c
Lucian Marin <marin.lucian@gmail.com>
2022-08-25 00:26:27 +0300

    font name fix

--- num/index.php
+++ num/index.php
@@ -12,7 +12,7 @@
 	<link rel="apple-touch-icon" type="image/png" href="static/num.png">
 	<link rel="icon" sizes="192x192" type="image/png" href="static/num.png">
 	<link rel="manifest" type="application/json" href="static/manifest.json">
-	<link rel="stylesheet" type="text/css" href="static/style.css?v=9">
+	<link rel="stylesheet" type="text/css" href="static/style.css?v=10">
 	<script type="text/javascript" src="static/math.js"></script>
 	<script type="text/javascript" src="static/script.js?v=4"></script>
 </head>
